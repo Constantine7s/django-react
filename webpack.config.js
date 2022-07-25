8ec62c030f622229b83859b1fc63ec5755d5af55
Konstantin Shambazov <k.shambazov@gmail.com>
2022-07-25 15:29:36 +0900

    feat: update app for react

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     filename: 'index-bundle.js',
     path: path.resolve(__dirname, './static')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      }, 
+
+    ]
   }
 };
